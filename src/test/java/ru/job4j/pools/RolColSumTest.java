680d7d6cf1562b76a406be7388b34d423c0097ab
markmw <dant1kkz@gmail.com>
2022-08-08 17:48:07 +0600

    4. CompletableFuture [#361626 #330002]

--- src/test/java/ru/job4j/pools/RolColSumTest.java
+++ src/test/java/ru/job4j/pools/RolColSumTest.java
@@ -24,24 +24,24 @@ class RolColSumTest {
     @Test
     public void whenSequentialSumThenArray() {
         int[][] matrix = new int[][]{new int[]{1, 2}, new int[]{3, 4}};
-        RolColSum.Sums[] rsl = new RolColSum.Sums[]{new RolColSum.Sums(3, 4)
-                , new RolColSum.Sums(7, 6)};
+        RolColSum.Sums[] rsl = new RolColSum.Sums[]{new RolColSum.Sums(3, 4),
+                new RolColSum.Sums(7, 6)};
         assertThat(rsl).isEqualTo(RolColSum.sum(matrix));
     }
 
     @Test
     public void whenAsyncSumThenArray() throws ExecutionException, InterruptedException {
         int[][] matrix = new int[][]{new int[]{1, 2}, new int[]{3, 4}};
-        RolColSum.Sums[] rsl = new RolColSum.Sums[]{new RolColSum.Sums(3, 4)
-                , new RolColSum.Sums(7, 6)};
+        RolColSum.Sums[] rsl = new RolColSum.Sums[]{new RolColSum.Sums(3, 4),
+                new RolColSum.Sums(7, 6)};
         assertThat(rsl).isEqualTo(RolColSum.asyncSum(matrix));
     }
 
     @Test
     public void whenAsyncSumMatrixLengthThreeThenArray() throws ExecutionException, InterruptedException {
         int[][] array = new int[][]{new int[]{1, 2, 3}, new int[]{4, 5, 6}, new int[]{7, 8, 9}};
-        RolColSum.Sums[] rsl = new RolColSum.Sums[]{new RolColSum.Sums(6, 12)
-                , new RolColSum.Sums(15, 15), new RolColSum.Sums(24,18)};
+        RolColSum.Sums[] rsl = new RolColSum.Sums[]{new RolColSum.Sums(6, 12),
+                new RolColSum.Sums(15, 15), new RolColSum.Sums(24,18)};
         assertThat(rsl).isEqualTo(RolColSum.asyncSum(array));
     }
 }
